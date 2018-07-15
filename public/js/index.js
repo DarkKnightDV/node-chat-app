831589e901de2beeab59acc16b5e7e4b5e152eae
Pankaj Pandey <pp030203@gmail.com>
2018-07-15 20:51:34 +0800

    Added socket check and emit message

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+var socket = io();
+
+socket.on("connect", function () {
+    console.log("Server connected");
+
+    socket.emit('createMessage', {
+        from: "jen@gmail.com",
+        text: "Can we meet tomorrow instead?"
+    });
+});
+socket.on('disconnect', function () {
+    console.log("Server disconnected.");
+});
+
+socket.on('newMessage', function(message){
+    console.log("New Message", message);
+}) ;
--- server/server.js
+++ server/server.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 3000;
+const http = require('http');
+const socketIO =  require('socket.io');
 
+const PORT = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var  io = socketIO(server);
 
 var publicPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicPath));
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+    console.log("Client Connected");
+
+    socket.on('disconnect', () => {
+        console.log("Client Disconnected");
+    })
+
+    socket.on('createMessage', (message) => {
+        console.log("Create Message", message);
+    });
+
+    socket.emit("newMessage", {
+        from: "deep@gmail.com",
+        text: "I am available at 4:00 PM. How about you?",
+        createdAt: new Date().toDateString()
+    });
+});
+
+
+server.listen(PORT, () => {
     console.log(`Server started on PORT ${PORT}`);
 })
