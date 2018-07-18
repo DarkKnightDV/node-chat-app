49150649ad0aad8c6c41a92bd36b74806bbc862f
Pankaj Pandey <pp030203@gmail.com>
2018-07-18 12:37:10 +0800

    Added chat window height calculation

--- public/js/index.js
+++ public/js/index.js
@@ -23,6 +23,7 @@ socket.on('newMessage', function(message){
         completedAt: moment(message.completedAt).format('h:mm a')
     });
     $("#messages").append(html);
+    scrollToBottom ();
 }) ;
 
 socket.on("newLocationMessage", function (message){
@@ -40,7 +41,23 @@ socket.on("newLocationMessage", function (message){
         // text: `<a target="_blank" href=${message.url}>My Location</a>`
     });
     $("#messages").append(html);
+    scrollToBottom ();
 });
+
+function scrollToBottom () {
+    var messages = $('#messages');
+    var lastMessage = $(messages).children('li:last-child');
+
+    var scrollHeight = $(messages).prop('scrollHeight');
+    var scrollTop = $(messages).prop('scrollTop');
+    var clientHeight = $(messages).prop('clientHeight');
+    var newMessageHeight = $(lastMessage).innerHeight();
+    var prevMsgHeight = $(lastMessage).prev().innerHeight();
+
+    if((clientHeight + scrollTop + newMessageHeight + prevMsgHeight) >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+};
 
 $('#message-form').on("submit", function(ev) {
     ev.preventDefault();
@@ -61,12 +78,12 @@ $("#send-location").on("click", function () {
         return alert("Sorry, geolocation not supported by browser");
     }
     $(sendLocBtn).attr('disabled', "true").text('Sending location...');
-    navigator.geolocation.getCurrentPosition(function (position){
-        $(sendLocBtn).removeAttr('disabled').text('Send location');
-        socket.emit('createLocationMessage', {
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude
-        });
+        navigator.geolocation.getCurrentPosition(function (position){
+            $(sendLocBtn).removeAttr('disabled').text('Send location');
+            socket.emit('createLocationMessage', {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            });
     }, function () {
         $(sendLocBtn).removeAttr('disabled').text('Send location');
         alert("Unable to get geolocation.");
